e476dabc75cd4cae8f156e6d89fa7a8906f47a3b
wafflesGH <aafawaselgui@gmail.com>
2025-07-09 16:04:41 +0100

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,12 +1,11 @@
 const color = document.getElementById('colorDiv');
 
     color.addEventListener('mousemove', function(event) {
-        // Get the X and Y coordinates of the cursor relative to the element
+        // cursor position
         const x = event.offsetX;
         const y = event.offsetY;
-
-        // Calculate RGB values based on cursor position
-        // Modulo 255 ensures values stay within the 0-255 range for RGB
+        // *********
+        // values stay between 0-255 for rgb
         const red = x % 255;
         const green = y % 255;
         const blue = (x + y) % 255;
